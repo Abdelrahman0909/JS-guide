bf2b283933af7dc183af49d59f9947221f727d69
Abdelrahman0909 <you@example.com>
2021-01-12 17:26:29 +0200

    first commit to my repo

---
+++ app.js
@@ -0,0 +1,60 @@
+// const personFirstName = [{name: 'Max'}, {name: 'Manual'}, {name: 'Anna'}];
+// console.log(personFirstName.find((person, idx, persons)=>{
+//     return person.name === 'Anna';
+// }));
+
+
+
+// const myArr = Array.from('foo');
+// console.log(myArr);
+
+
+// const mySet = new Set(['Hi', 'From', 'My', 'PC', 1]);
+// for(const word of mySet){
+//     console.log(word);
+// }
+
+
+// const p1 = {name: 'max'};
+// const p2 = {name: 'jon'};
+
+// const personData = new Map([[p1, {age: 20}], [p2, {age: 19}]]);
+
+// for(const key of personData.keys()){
+//     console.log(key);
+// }
+
+// for(const val of personData.values()){
+//     console.log(val);
+// }
+
+// for(const entries of personData.entries()){
+//     console.log(entries);
+// }  
+
+
+// const personData1 = {
+//     name: 'Mark',
+//     age: 20,
+//     hieght: 180,
+//     'hair color': 'red'
+// };
+
+// const personData2 = {
+//     name: 'John',
+//     age: 23,
+//     hieght: 190,
+//     'hair color': 'black'
+// }
+
+// const arr = [20, 30, 48, 99, 19, '20'];
+// const Data = Object.assign({}, personData1);
+// const Data2 = {...personData2};
+// console.log(Data, Data2);
+
+// const Age = [...arr];
+// const { name, age, ...other } = personData1
+// console.log(name, age, other);
+// console.log(Age);
+
+
